170a330da772784acb7d446d4f07dd37de65d123
adityag30 <160811838+adityag30@users.noreply.github.com>
2025-02-21 23:36:29 +0530

    Update Pattern7.java

--- Pattern7.java
+++ Pattern7.java
@@ -21,4 +21,14 @@ public class Pattern7 {
         }
     }
 }
-
+/*
+   may also use
+      for(int i=1;i<=n;i++)
+      {
+         for(int j=1;j<=n-i+1;j++)
+         {
+            System.out.print(j+" ");
+         }
+         System.out.println();
+      }   
+*/
