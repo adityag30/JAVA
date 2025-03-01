404a0331e330d07f40ece100e191a3657add3286
adityag30 <160811838+adityag30@users.noreply.github.com>
2025-03-01 16:32:41 +0530

    Update Function1.java

--- Functions/Function1.java
+++ Functions/Function1.java
@@ -1,1 +1,14 @@
+import java.util.*;
+public class Function1 {
+    public static int product(int x,int y){
+        return x*y;
+    }
+    public static void main(String[]args){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter The Numbers--");
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        System.out.println("The Product of the Numbers is--"+product(a,b));
+    }
+}
 
